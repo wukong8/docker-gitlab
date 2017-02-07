61c73f05781468d4483fdd32ae4418bd7b4ffd76
cjj8110 <cjj8110@163.com>
2017-02-07 17:24:38 +0800

    fixed:change to alpine

--- assets/build/run_install.sh
+++ assets/build/run_install.sh
@@ -32,8 +32,8 @@ exec_as_git() {
 sed -i '1i\http://mirrors.ustc.edu.cn/alpine/v3.5/main\nhttp://mirrors.ustc.edu.cn/alpine/v3.5/community' /etc/apk/repositories
 apk add --update wget curl gcc g++ make patch cmake linux-headers tzdata python2 supervisor git gettext go nodejs autoconf bison coreutils procps sudo yaml-dev gdbm-dev zlib-dev readline-dev libc-dev ncurses-dev libffi-dev libxml2-dev libxslt-dev icu-dev mysql-dev postgresql-dev ruby-dev ruby-irb ruby-rdoc ruby-bundler ruby-bigdecimal
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
-gem update --system
-gem install --no-document bundler rdoc-data
+gem update --system --no-document
+gem install --no-document bundler rdoc-data rake tzinfo
 echo "Coping assets..."
 mkdir -p ${GITLAB_BUILD_DIR}
 cp -R /root/docker-gitlab/assets/build/ ${GITLAB_BUILD_DIR}/
