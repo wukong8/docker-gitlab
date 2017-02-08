cbb6814472bdbcf2d0709a4b841a3cf11b64c3e0
cjj8110 <cjj8110@163.com>
2017-02-08 20:44:50 +0800

    fixed:change to alpine

--- assets/build/run_install.sh
+++ assets/build/run_install.sh
@@ -42,10 +42,10 @@ sed -i '1i\http://mirrors.ustc.edu.cn/alpine/v3.5/main\nhttp://mirrors.ustc.edu.
 apk add --update wget curl gcc g++ make patch cmake linux-headers tzdata python2 supervisor git gettext go nodejs autoconf bison coreutils procps sudo yaml-dev gdbm-dev zlib-dev readline-dev libc-dev ncurses-dev libffi-dev libxml2-dev libxslt-dev icu-dev mysql-dev postgresql-dev ruby-dev ruby-irb ruby-rdoc ruby-bundler ruby-bigdecimal
 gem sources --add https://gems.ruby-china.org/ --remove https://rubygems.org/
 gem update --system --no-document
-gem install --no-document bundler
+gem install --no-document bundler rake
 mkdir -p ${GEM_CACHE_DIR}
 #gem install -i ${GEM_CACHE_DIR} --no-document rake rdoc-data tzinfo rainbow haml_lint
-gem install -i ${GEM_CACHE_DIR} --no-document rake rdoc-data tzinfo haml_lint
+gem install -i ${GEM_CACHE_DIR} --no-document rdoc-data tzinfo haml_lint
 echo "Coping assets..."
 mkdir -p ${GITLAB_BUILD_DIR}
 cd ${GITLAB_BUILD_DIR}
